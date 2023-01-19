29902ae413f5b9700a8b3a77742f4d9daddae0bf
Alexershov86 <alexershov86@mail.ru>
2023-01-19 11:10:32 +0300

    ДЗ_13 v.05

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -5,14 +5,13 @@
 
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите цифру числа: ");
-int num = Convert.ToInt32(Console.ReadLine());
+int num = 3;
 
 void TakeNumber(int number, int num)
 {
     if (number.ToString().Length >= num)
         Console.WriteLine(number + " -> " + number.ToString()[num - 1]);
-    else Console.WriteLine(number + " -> такой цифры нет");
+    else Console.WriteLine(number + " -> третьей цифры нет");
 }
 
 TakeNumber(number, num);
