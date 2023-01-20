0f0dfac77d7a6a9c99e4a84021aaaf4c5a5f7453
Alexershov86 <alexershov86@mail.ru>
2023-01-20 23:50:42 +0300

    Задача 19 int версия

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -8,12 +8,26 @@
 bool IsPalindrome(int number)
 {
     int count = 0;
-    for (int i = 0, j = number.ToString().Length - 1; i < j; i++, j--)
+    int clone_number = number;
+    for (int i = 0; clone_number > 0; i++)
     {
-        if (number.ToString()[i] == number.ToString()[j])
+        clone_number /= 10;
+        count++;
+    }
+    int[] number_array = new int[count];
+    for (int i = number_array.Length - 1; number > 0; i--)
+    {
+        number_array[i] = number % 10;
+        number /= 10;
+    }
+
+    count = 0;
+    for (int i = 0, j = number_array.Length - 1; i < j; i++, j--)
+    {
+        if (number_array[i] == number_array[j])
             count++;
     }
-    if (count == number.ToString().Length / 2)
+    if (count == number_array.Length / 2)
         return true;
     else
         return false;
