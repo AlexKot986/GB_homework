79f960f8ec1d91cd0f53fd7b6cbc49827801f30e
Alexershov86 <alexershov86@mail.ru>
2023-01-21 22:58:37 +0300

    Задача 21 v.02

--- Task_21/Program.cs
+++ Task_21/Program.cs
@@ -5,30 +5,26 @@ A (7,-5, 0); B (1,-1,9) -> 11.53   */
 
 //D=sqrt((X2-X1)^2+(Y2-Y1)^2+(Z2-Z1)^2)
 
-Random rand = new Random();
+Random random = new Random();
 
-int x1 = rand.Next(-100, 101), y1 = rand.Next(-100, 101), z1 = rand.Next(-100, 101);
-int x2 = rand.Next(-100, 101), y2 = rand.Next(-100, 101), z2 = rand.Next(-100, 101);
+double TwoPointLenght(int[] a, int[] b)
+{
+    double length = Math.Sqrt(Math.Pow(a[0] - b[0], 2) + Math.Pow(a[1] - b[1], 2) + Math.Pow(a[2] - b[2], 2));
+    return length;
+}
+Console.WriteLine("Введите координаты точки A: ");
+int[] point_a = {Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())};
+int ax = Convert.ToInt32(point_a[0]);
+int ay = Convert.ToInt32(point_a[1]);
+int az = Convert.ToInt32(point_a[2]);
 
-Console.WriteLine($"Point A  ({x1}, {y1}, {z1})");
-Console.WriteLine($"Point B  ({x2}, {y2}, {z2})");
+Console.WriteLine("Введите координаты точки B: ");
+int[] point_b = {random.Next(-100, 101), random.Next(-100, 101), random.Next(-100, 101)};
+int bx = Convert.ToInt32(point_b[0]);
+int by = Convert.ToInt32(point_b[1]);
+int bz = Convert.ToInt32(point_b[2]);
 
-double length = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
-Console.WriteLine(Math.Round(length,2));
+Console.WriteLine($"Точка A  ({ax}, {ay}, {az})");
+Console.WriteLine($"Точка B  ({bx}, {by}, {bz})");
 
-
-
-Console.WriteLine("Введите координаты точки 1: ");
-string[] sArr = Console.ReadLine().Split(' ');
-int a1 = Convert.ToInt32(sArr[0]);
-int b1 = Convert.ToInt32(sArr[1]);
-int c1 = Convert.ToInt32(sArr[2]);
-
-Console.WriteLine("Введите координаты точки 2: ");
-string[] sArr2 = Console.ReadLine().Split(' ');
-int a2 = Convert.ToInt32(sArr2[0]);
-int b2 = Convert.ToInt32(sArr2[1]);
-int c2 = Convert.ToInt32(sArr2[2]);
-
-//double result = ;
-Console.Write("{0:#.00}", Math.Sqrt(Math.Pow((a1 - a2), 2) + Math.Pow((b1 - b2), 2) + Math.Pow((c1 - c2), 2)));
+Console.Write("{0:#.00}", TwoPointLenght(point_a, point_b));
