46cc426106fbe0ce739a05dfab52c9d089d6b5ee
Alexershov86 <alexershov86@mail.ru>
2023-02-08 00:05:30 +0300

    Задача 54 v.01

---
+++ Task_54/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] CreateArray()
+{
+    Random random = new Random();
+    int[,] array = new int[random.Next(3, 6), random.Next(3, 6)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(1, 6);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j],4}");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] SortArray(int[,] array)
+{
+    int temp;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int k = 0; k < array.GetLength(1) - 1; k++)
+            for (int j = 0; j < array.GetLength(1) - 1 - k; j++)
+            {
+                if (array[i, j] < array[i, j + 1])
+                {
+                    temp = array[i, j];
+                    array[i, j] = array[i, j + 1];
+                    array[i, j + 1] = temp;
+                }
+            }
+    }
+    return array;
+}
+int[,] arr = CreateArray();
+PrintArray(arr);
+PrintArray(SortArray(arr));
