ca6e6b0bf67f75c5f2afe701adae9b704fa5d254
Alexershov86 <alexershov86@mail.ru>
2023-02-11 00:06:10 +0300

    Задача 68 v.02 (&& -> &)

--- Task_68/Program.cs
+++ Task_68/Program.cs
@@ -10,7 +10,7 @@ int n = random.Next(0, 6);
 int Ackermann(int m, int n)
 {
     if (m == 0) return n + 1;
-    if (m > 0 && n == 0) return Ackermann(m - 1, 1);
+    if (m > 0 & n == 0) return Ackermann(m - 1, 1);
     return Ackermann(m - 1, Ackermann(m, n - 1));
 }
 
