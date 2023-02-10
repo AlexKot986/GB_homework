88226e8e87d900eaa4152cb06041fe6eb7bb45f3
Alexershov86 <alexershov86@mail.ru>
2023-02-10 23:05:05 +0300

    Задача 66 v.01

---
+++ Task_66/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая 
+// найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNumbers(int numFirst, int numSecond)
+{
+    if (numFirst == numSecond) return numSecond;
+    return numFirst + SumNumbers(numFirst + 1, numSecond);
+}
+int firstNum = new Random().Next(0, 11);
+int secondNum = new Random().Next(0, 11);
+
+int min;
+int max;
+if (firstNum < secondNum) {min = firstNum; max = secondNum;}
+else {max = firstNum; min = secondNum;}
+
+Console.WriteLine($"Сумма чисел от {min} до {max} = -> {SumNumbers(min, max)}");
